67c7eca96196b9abd71f6d88bb9d4dee6b5ee70e
xschahl <xavier.schahl@epitech.eu>
2023-04-11 19:39:55 +0200

    [ADD] Server data

---
+++ data/resources/[loadingscreen]/loadingscreen/config.js
@@ -0,0 +1,11 @@
+//-----------------Config-----------------
+var title = "GTRP_FIVE";
+var link = "https://discord.gg/K7hs8v3MpM";
+//---------------End Config---------------
+
+
+document.getElementById('title').innerHTML = title;
+document.getElementById('link').innerHTML = link;
+ 
+
+console.log('Connected.')
